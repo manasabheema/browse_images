6553a1406e3183a7949e21e862ec621ec57c483c
Manasa <50434753+manasabheema@users.noreply.github.com>
2021-08-26 21:55:36 +0530

    Update App.js
    Second commit - Handling no record found case !

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,35 @@ import LandingText from "./LandingText";
 import Footer from "./Footer";
 
 class App extends React.Component {
-  state = { images: [], showLandingText: true };
+  state = { images: [], showLandingText: true, imageData: null };
 
   onSearchSubmit = async (term) => {
     const response = await unsplash.get("/search/photos", {
       params: { query: term },
     });
+
     this.setState({ images: response.data.results, showLandingText: false });
+    if (this.state.images.length) {
+      this.setState({ imageData: true });
+    } else {
+      this.setState({ imageData: false });
+    }
   };
 
   render() {
     return (
-      <div className="ui container">
-        <SearhBar onSubmit={this.onSearchSubmit} />
-        {this.state.showLandingText ? <LandingText /> : null}
-        <ImageList images={this.state.images} />
+      <div className="flex-wrapper">
+        <div className="ui container">
+          <SearhBar onSubmit={this.onSearchSubmit} />
+          {this.state.showLandingText ? <LandingText /> : null}
+          {this.state.imageData === false ? (
+            <div className="landing-text" style={{ color: "#F71616" }}>
+              Uff... No search found. Try something else !
+            </div>
+          ) : (
+            <ImageList images={this.state.images} />
+          )}
+        </div>
         <Footer />
       </div>
     );
